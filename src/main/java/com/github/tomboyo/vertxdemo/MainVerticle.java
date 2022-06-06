7663edf9e203f56ca47061d49ccbd4f1722eec00
Tom Simmons <thsimmon@redhat.com>
2022-06-05 20:40:19 -0400

    Refactor (composition instead of callbacks)

--- src/main/java/com/github/tomboyo/vertxdemo/MainVerticle.java
+++ src/main/java/com/github/tomboyo/vertxdemo/MainVerticle.java
@@ -2,8 +2,12 @@ package com.github.tomboyo.vertxdemo;
 
 import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.JksOptions;
 
@@ -14,25 +18,29 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     ConfigRetriever.create(vertx)
-        .getConfig(ar -> {
-          var config = ar.result();
+    .getConfig()
+    .compose(this::launchHttpServer)
+    .onSuccess(httpServer -> {
+      startPromise.complete();
+      port = httpServer.actualPort();
+      System.out.println("HTTP server started on port " + port);
+    })
+    .onFailure(cause -> {
+      startPromise.fail(cause);
+    });
+  }
 
-          var portConfig = config.getInteger("main.verticle.http.port", 8080);
-          vertx.createHttpServer(httpServerOptions(config))
-              .requestHandler(req -> {
-                req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!");
-              }).listen(portConfig, http -> {
-                if (http.succeeded()) {
-                  startPromise.complete();
-                  port = http.result().actualPort();
-                  System.out.println("HTTP server started on port " + port);
-                } else {
-                  startPromise.fail(http.cause());
-                }
-              });
-        });
+  private Future<HttpServer> launchHttpServer(JsonObject config) {
+    var portConfig = config.getInteger("main.verticle.http.port", 8080);
+    return vertx.createHttpServer(httpServerOptions(config))
+              .requestHandler(this::defaultHandler)
+              .listen(portConfig);
+  }
+
+  private void defaultHandler(HttpServerRequest req) {
+      req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!");
   }
 
   public int port() {

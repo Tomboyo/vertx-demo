533b63fea441a93e4066308250f82a7ff6858191
Tom Simmons <thsimmon@redhat.com>
2022-06-05 20:23:40 -0400

    Add SSL Hello test

--- src/main/java/com/github/tomboyo/vertxdemo/MainVerticle.java
+++ src/main/java/com/github/tomboyo/vertxdemo/MainVerticle.java
@@ -9,27 +9,37 @@ import io.vertx.core.net.JksOptions;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private int port = -1;
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     ConfigRetriever.create(vertx)
-      .getConfig(ar -> {
-        var config = ar.result();
+        .getConfig(ar -> {
+          var config = ar.result();
 
-        var port = config.getInteger("main.verticle.http.port", 8080);
-        vertx.createHttpServer(httpServerOptions(config))
-          .requestHandler(req -> {
-            req.response()
-              .putHeader("content-type", "text/plain")
-              .end("Hello from Vert.x!");
-          }).listen(port, http -> {
-            if (http.succeeded()) {
-              startPromise.complete();
-              System.out.println("HTTP server started on port " + port);
-            } else {
-              startPromise.fail(http.cause());
-            }
-          });
-      });
+          var portConfig = config.getInteger("main.verticle.http.port", 8080);
+          vertx.createHttpServer(httpServerOptions(config))
+              .requestHandler(req -> {
+                req.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello from Vert.x!");
+              }).listen(portConfig, http -> {
+                if (http.succeeded()) {
+                  startPromise.complete();
+                  port = http.result().actualPort();
+                  System.out.println("HTTP server started on port " + port);
+                } else {
+                  startPromise.fail(http.cause());
+                }
+              });
+        });
+  }
+
+  public int port() {
+    if (port == -1) {
+      throw new IllegalStateException("Port cannot be retrieved because the verticle has not started");
+    }
+    return port;
   }
 
   private HttpServerOptions httpServerOptions(JsonObject config) {
--- src/test/java/com/github/tomboyo/vertxdemo/TestMainVerticle.java
+++ src/test/java/com/github/tomboyo/vertxdemo/TestMainVerticle.java
@@ -2,9 +2,16 @@ package com.github.tomboyo.vertxdemo;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.RequestOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.PemTrustOptions;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+
+import static io.vertx.core.http.HttpMethod.GET;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,18 +20,35 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
+  private MainVerticle verticle;
+
   @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+  void deployVerticle(Vertx vertx, VertxTestContext testContext) throws Exception {
+    verticle = new MainVerticle();
     vertx.deployVerticle(
-        new MainVerticle(),
+        verticle,
         new DeploymentOptions().setConfig(
             new JsonObject()
-              .put("main.verticle.p12.path", "etc/certs/server.p12")),
-        testContext.succeeding(id -> testContext.completeNow()));
+                .put("main.verticle.http.port", 0)
+                .put("main.verticle.p12.path", "etc/certs/server.p12")),
+        testContext.succeedingThenComplete());
   }
 
   @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
+  void sayHello(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    vertx.createHttpClient(new HttpClientOptions()
+        .setPemTrustOptions(new PemTrustOptions()
+            .addCertPath("etc/certs/cert.pem")))
+        .request(new RequestOptions()
+            .setSsl(true)
+            .setPort(verticle.port())
+            .setHost("localhost")
+            .setMethod(GET)
+            .setURI("/"))
+        .compose(req -> req.send().compose(HttpClientResponse::body))
+        .onComplete(testContext.succeeding(buffer -> testContext.verify(() -> {
+          assertEquals("Hello from Vert.x!", buffer.toString());
+          testContext.completeNow();
+        })));
   }
 }
